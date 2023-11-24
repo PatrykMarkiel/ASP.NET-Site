e6f353982470c3cd88825ac18bebc0afd0a420ee
Bułas <103049804+PatrykMarkiel@users.noreply.github.com>
2023-11-24 03:25:41 +0100

    The Extensions folder has been added and now has an extension for displaying the names of the enum used in the Profile Index

---
+++ Models/ViewModel/MeasurementVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebApplication3.Data.Entities;
+
+namespace WebApplication3.ViewModels
+{
+    public class MeasurementVm
+    {
+        public int MeasurementId { get; set; }
+        public string MeasurementName { get; set; }
+        public string MeasurementComment { get; set; }
+        public DateTime MeasurementTreatmentTime { get; set; }
+        public DateTime MeasurementInsertionTime { get; set; }
+        public double MeasurementValue { get; set; }
+        public BodyPart MeasurementBodyPartName { get; set; }
+        public string MeasurementUserId { get; set; }
+
+        public Guid TestTypeId { get; set; }
+        public string TestTypeName { get; set; }
+        public string TestTypeDescription { get; set; }
+        public string TestTypeComment { get; set; }
+        public DateTime TestTypeSafeRange { get; set; }
+        public double TestTypeValue { get; set; }
+        public double TestTypeValueTemplate { get; set; }
+        public TestName TestTypeMeasurementName { get; set; }
+    }
+}
