ad6c2106ccbed4c8e9d7562ca8e668ef45ac2370
Bułas <103049804+PatrykMarkiel@users.noreply.github.com>
2023-11-30 01:57:21 +0100

    I added heart rate display in the dashboard

---
+++ Models/ViewModel/HomeVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApplication3.Data.Entities;
+
+namespace WebApplication3.Models.ViewModels
+{
+    public class DeviceMeasurementProfileViewModel
+    {
+        public List<DeviceEntity> Devices { get; set; }
+        public List<MeasurementEntity> Measurements { get; set; }
+        public ProfileEntity Profile { get; set; }
+    }
+}
